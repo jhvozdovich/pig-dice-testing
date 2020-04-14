4edf4dc40f3c738f77854fdc424d0c5aac2f7895
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-14 14:09:45 -0700

    add round total addition to grand total test

--- __tests__/pig-dice.test.js
+++ __tests__/pig-dice.test.js
@@ -1,6 +1,6 @@
 import { PlayerResults } from "../src/pig-dice.js";
 import { eachRoll } from "../src/pig-dice.js";
-// import { turnTotal } from "../src/pig-dice.js";
+import { turnTotal } from "../src/pig-dice.js";
 
 describe ("PlayerResults", () => {
   test("should correctly initialize a game with player totals of 0, a round of 1, and starting with player 1", () => {
@@ -26,3 +26,28 @@ describe ("switchPlayer", () => {
     expect(playerResults.player1Turn).toEqual(false);
   });
 });
+
+describe ("updateTotal", () => {
+  test("should add player's round to their grand total", () => {
+    var playerResults = new PlayerResults();
+    playerResults.updateTotal(2);
+    expect(playerResults.player1GrandTotal).toEqual(2);
+  });
+});
+
+
+
+// PlayerResults.prototype.updateTotal = function(turnTotal) {
+//   if (this.player1Turn) {
+//     this.player1GrandTotal += turnTotal;
+//     this.switchPlayer();
+//   } else {
+//     this.player2GrandTotal += turnTotal;
+//     this.round += 1;
+//     if (this.round === 4) {
+//       endOfGame();
+//     } else {
+//       this.switchPlayer();
+//     }
+//   }
+// };
